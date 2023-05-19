b58713ef2fd56b82243a6b5a31fb669856c6ed76
ni <profe22801@gmail.com>
2023-05-19 20:00:43 +0300

    из views удален index4

--- woman/views.py
+++ woman/views.py
@@ -12,8 +12,6 @@ def index2(request):
     posts = Articls.objects.all()
     return render(request, 'woman/index2.html', {'posts': posts, })
 
-def index4(request):
-    return 'aboba'
 
 def create(request):
     if request.method == 'POST':
